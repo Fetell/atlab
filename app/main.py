4db70445d296227d7362b17757a05a38cc5e2305
Fetell <fetell.eee@gmail.com>
2025-07-28 07:13:29 +0300

    Add Jinja2, update homepage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
 # from pydantic import BaseModel
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from .models import Base, Product, SessionLocal, engine
 
 app = FastAPI()
-
-# class Product(BaseModel):
-#     name: str
-#     price: float
-#     in_stock: bool
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.on_event("startup")
 def startup():
@@ -25,15 +24,13 @@ def get_db():
         db.close()
 
 @app.get("/", response_class = HTMLResponse)
-async def home():
-    return """
-    <html>
-        <body>
-            <h1 style="text-align:center;">My Online Shop</h1>
-            <a style="text-align:center;" href="/docs">API Docs</a>
-        </body>
-    </html>
-    """
+async def home(request: Request, db: Session = Depends(get_db)):
+    products = db.scalars(select(Product)).all()
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "products": products}
+    )
+
 
 @app.get("/products/{product_id}")
 async def read_product(product_id: int, db: Session = Depends(get_db)):
